1bf4d25fbd2cd0193a635095521d794bff958e62
doge <faa31.31.31.31@mail.ru>
2021-03-30 14:44:57 +0500

    Add filter and other, 1.02

--- com/sunlight/instruments/Constants.java
+++ com/sunlight/instruments/Constants.java
@@ -4,12 +4,12 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Constants {
     /***
-     Класс с константами для работы с другими классами.
-
+     A class with constants for working with other classes.
      @author aftern0on
+     @version 1.00
      ***/
 
-    // Положения, которые могут принимать границы
+    /** Provisions that borders can accept **/
     public enum Position {
         TOP(new Vector2(0, 1), new Vector2(1, 1)),
         RIGHT(new Vector2(1, 0), new Vector2(1, 0)),
@@ -17,16 +17,24 @@ public class Constants {
         LEFT(new Vector2(0, 0), new Vector2(0, 1));
 
         public Vector2 start, end;
-
         Position(Vector2 start, Vector2 end) {
             this.start = start;
             this.end = end;
         }
+
+        /** Reverse current value **/
+        public Position reverse() {
+            if (this == TOP) return DOWN;
+            if (this == DOWN) return TOP;
+            if (this == LEFT) return RIGHT;
+            if (this == RIGHT) return LEFT;
+            return null;
+        }
     }
 
-    // Стороны и направления
-    // Хранят отступы от центрального положения (x, y) для координации в матрицах
-    public enum Directions {
+    /** Parties and directions
+     * Store the margins from the central position (x, y) for coordination in matrices **/
+    public enum Direction {
         TOP_LEFT(new float[] {-1, -1}),
         TOP(new float[] {0, -1}),
         TOP_RIGHT(new float[] {1, -1}),
@@ -39,6 +47,21 @@ public class Constants {
         DOWN_RIGHT(new float[] {1, 1});
 
         public float[] direction;
-        Directions(float[] direction) { this.direction = direction; }
+        Direction(float[] direction) { this.direction = direction; }
+
+        /** Reverse current value **/
+        public Direction reverse() {
+            if (this == TOP_LEFT) return DOWN_RIGHT;
+            if (this == TOP) return DOWN;
+            if (this == TOP_RIGHT) return DOWN_LEFT;
+
+            if (this == CENTER_LEFT) return CENTER_RIGHT;
+            if (this == CENTER_RIGHT) return DOWN_LEFT;
+
+            if (this == DOWN_LEFT) return TOP_RIGHT;
+            if (this == DOWN) return TOP;
+            if (this == DOWN_RIGHT) return TOP_LEFT;
+            return null;
+        }
     }
 }
